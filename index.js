a28167438d2dc283c982eebfb7a0b0a7e0969780
Cgg <clement.geiger@gmail.com>
2021-04-07 14:51:14 +0200

    factorize setEntryText

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const padEntry = document.getElementById("padEntry");
 const btnClass = "button";
 const validKeys = "1234567890*#abcd";
 
+function setEntryText(text) {
+  padEntry.value = text;
+}
+
 padEntry.addEventListener("keydown", (e) => {
   if (e.key !== "Tab") {
     e.preventDefault();
@@ -10,7 +14,7 @@ padEntry.addEventListener("keydown", (e) => {
 
 document.addEventListener("mousedown", ({ target }) => {
   if (target.classList.contains(btnClass)) {
-    padEntry.value += target.innerText;
+    setEntryText(padEntry.value + innerText);
   }
 });
 
@@ -23,13 +27,13 @@ document.addEventListener("keydown", (e) => {
 
   if (validKeys.indexOf(key) !== -1) {
     const entry = key.toUpperCase();
-    padEntry.value += entry;
+    setEntryText(padEntry.value + entry);
 
     if (document.activeElement !== padEntry) {
       document.getElementById(entry).focus();
     }
   } else if (key === "Backspace" && padEntry.value.length > 0) {
-    padEntry.value = padEntry.value.slice(0, -1);
+    setEntryText(padEntry.value.slice(0, -1));
 
     if (document.activeElement !== padEntry) {
       document.activeElement.blur();
@@ -37,7 +41,7 @@ document.addEventListener("keydown", (e) => {
   } else if (key === " ") {
     const activeElement = document.activeElement;
     if (activeElement.classList.contains(btnClass)) {
-      padEntry.value += activeElement.innerText;
+      setEntryText(padEntry.value + activeElement.innerText);
     }
   }
 });
